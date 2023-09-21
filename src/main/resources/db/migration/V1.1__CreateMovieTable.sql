fdbe42c70f5ca39daab1a88d389c8591947b6093
VijayaPrashanth <vijaya.prashanth500@gmail.com>
2023-09-21 18:14:49 +0530

    Add. migration for creating movie table

---
+++ src/main/resources/db/migration/V1.1__CreateMovieTable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE MOVIE(ID VARCHAR(100),
+                    NAME VARCHAR(100) NOT NULL,
+                    RATING VARCHAR(15) NOT NULL,
+                    GENRE VARCHAR(100) NOT NULL,
+                    RUNNING_TIME_IN_MINS INT NOT NULL,
+                    IMAGE_URL VARCHAR(500) NOT NULL,
+                    PRIMARY KEY(ID));
